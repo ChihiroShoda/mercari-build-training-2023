df65b405b6677e08e355448e76e3a961959c6536
Chihiro Shoda <shoda-chihiro453@g.ecc.u-tokyo.ac.jp>
2023-05-12 16:38:55 +0900

    step3の2

--- python/main.py
+++ python/main.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import pathlib
+import json
 from fastapi import FastAPI, Form, HTTPException
 from fastapi.responses import FileResponse
 from fastapi.middleware.cors import CORSMiddleware
@@ -23,9 +24,18 @@ def root():
     return {"message": "Hello, world!"}
 
 @app.post("/items")
-def add_item(name: str = Form(...)):
-    logger.info(f"Receive item: {name}")
-    return {"message": f"item received: {name}"}
+def add_item(name: str = Form(...), category: str = Form(...)):
+    logger.info(f"Receive item: {name} (category: {category})")
+
+    # jsonファイルの読み込み / 書き込み
+    with open('items.json') as f:
+        items = json.load(f)
+    items["items"].append({"name": name, "category": category})
+
+    with open('items.json', 'wt') as f:
+        json.dump(items, f)
+
+    return {"message": f"item received: {name} (category: {category})"}
 
 @app.get("/image/{image_filename}")
 async def get_image(image_filename):
